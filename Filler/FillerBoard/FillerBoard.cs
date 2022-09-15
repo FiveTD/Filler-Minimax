5a3847b0e5f6ff0e9ffd9e721679b342dc5bbd13
Tim Lawrence <timothyjl2002@gmail.com>
2022-09-15 06:28:06 -0600

    FillerBoard implementation

--- Filler/FillerBoard/FillerBoard.cs
+++ Filler/FillerBoard/FillerBoard.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data.Common;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using Minimax;
 
 namespace FillerBoard
@@ -9,12 +11,27 @@ namespace FillerBoard
         const byte DEF_COLORS = 6;
         const byte DEF_PLAYERS = 2;
 
-        private byte[,] board;
+        private readonly byte[,] board;
+        private readonly int x, y;
 
         private readonly byte numColors;
         private readonly byte numPlayers;
 
-        private byte lastColor;
+        private readonly Dictionary<byte, byte> playerColors = new();
+        private readonly Dictionary<byte, HashSet<Tuple<int, int>>> playerTerritory = new();
+
+        private struct MoveRec
+        {
+            public byte PrevColor;
+            public Tuple<int, int>[] Changed;
+
+            public MoveRec(byte prev, params Tuple<int, int>[] changed)
+            {
+                PrevColor = prev;
+                Changed = changed;
+            }
+        }
+        private readonly Stack<MoveRec> moveHistory = new();
         
         public byte Turn { get; set; }
         public int Score { get => throw new NotImplementedException(); }
@@ -24,11 +41,30 @@ namespace FillerBoard
 
         public FillerBoard(int x = DEF_SIZE, int y = DEF_SIZE, byte colors = DEF_COLORS, byte players = DEF_PLAYERS)
         {
+            this.x = x; this.y = y;
             board = new byte[x, y];
             numColors = colors;
             numPlayers = players;
 
-            
+            Generate();
+
+            for (byte p = 0; p < numPlayers; p++)
+            {
+                playerTerritory.Add(p, new());
+                playerTerritory[p].Add(PlayerCorner(p));
+            }
+        }
+
+        private void Generate()
+        {
+            Random rand = new();
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    board[i, j] = (byte)rand.Next(0, numColors);
+                }
+            }
         }
 
         public byte this[int x, int y]
@@ -36,14 +72,10 @@ namespace FillerBoard
             get { return board[x, y]; }
         }
 
-        public int Size(int dimension = 0)
+        public bool IsValid(byte color, byte player)
         {
-            return board.GetLength(dimension);
-        }
-
-        public bool IsValid(byte color)
-        {
-            return true;
+            var _c = PlayerCorner(player);
+            return !(playerColors.ContainsValue(color) || board[_c.Item1, _c.Item2] == color);
         }
 
         public void Move(int[] color)
@@ -53,7 +85,27 @@ namespace FillerBoard
 
         public void Move(byte color)
         {
-            //TODO: Move
+            if (!IsValid(color, Turn)) return;
+
+            var _c = PlayerCorner(Turn);
+            byte prevColor = board[_c.Item1, _c.Item2];
+
+            foreach (var sp in playerTerritory[Turn])
+            {
+               board[sp.Item1, sp.Item2] = color;
+            }
+
+            HashSet<Tuple<int, int>> affected = new();
+            foreach (var adj in Adjacent(Turn))
+            {
+                if (board[adj.Item1, adj.Item2] == color)
+                {
+                    playerTerritory[Turn].Add(adj);
+                    affected.Add(adj);
+                }
+            }
+
+            moveHistory.Push(new MoveRec(prevColor, affected.ToArray()));
 
             Turn++;
             if (Turn == numPlayers) Turn = 0;
@@ -63,24 +115,102 @@ namespace FillerBoard
         {
             Turn--;
             if (Turn == 255) Turn = numPlayers;
+
+            MoveRec prev = moveHistory.Pop();
+
+            foreach (var adj in prev.Changed)
+            {
+                playerTerritory[Turn].Remove(adj);
+            }
+            foreach (var sp in playerTerritory[Turn])
+            {
+                board[sp.Item1, sp.Item2] = prev.PrevColor;
+            }
         }
 
         public int Size(byte player)
         {
-            return 0;
+            return playerTerritory[player].Count;
+        }
+
+        public List<byte> ValidMoves(byte player)
+        {
+            List<byte> validMoves = new();
+            for (byte c = 0; c < numColors; c++)
+            {
+                if (IsValid(c, player)) validMoves.Add(c);
+            }
+            return validMoves;
         }
 
         public List<int[]> ValidMoves()
         {
-            return new List<int[]>();
+            List<int[]> validMoves = new();
+            foreach (byte move in ValidMoves(Turn))
+            {
+                validMoves.Add(new int[] { move });
+            }
+            return validMoves;
         }
 
-        public bool Won(out byte winnner)
+        public bool Won(out byte winner)
         {
-            winnner = 0;
-            return true;
+            int totalSize = 0;
+            int largestSize = 0;
+            winner = 0;
+
+            for (byte p = 0; p < numPlayers; p++)
+            {
+                int size = playerTerritory[p].Count;
+                totalSize += size;
+
+                if (size >= largestSize)
+                {
+                    largestSize = size;
+                    winner = p;
+                }
+            }
+
+            return totalSize == x * y; // all spaces captured = winner exists
         }
 
+        private Tuple<int, int> PlayerCorner(byte player)
+        {
+            return player switch
+            {
+                0 => Tuple.Create(0, y - 1),
+                1 => Tuple.Create(x - 1, 0),
+                2 => Tuple.Create(0, 0),
+                3 => Tuple.Create(x - 1, y - 1),
+                _ => Tuple.Create(0, 0),
+            };
+        }
+
+        private HashSet<Tuple<int, int>> Adjacent(byte player)
+        {
+            HashSet<Tuple<int, int>> adjacent = new();
+            foreach (var coord in playerTerritory[player])
+            {
+                void AddAdjacent(int xAdj, int yAdj)
+                {
+                    var adj = Tuple.Create(coord.Item1 + xAdj, coord.Item2 + yAdj);
+                    
+                    if (!playerTerritory[player].Contains(adj))
+                    {
+                        if (adj.Item1 >= 0 && adj.Item1 < x &&
+                            adj.Item2 >= 0 && adj.Item2 < y)
+                            adjacent.Add(adj);
+                    }
+                }
+
+                AddAdjacent(1, 0);
+                AddAdjacent(0, 1);
+                AddAdjacent(-1, 0);
+                AddAdjacent(0, -1);
+            }
+
+            return adjacent;
+        }
 
         //public FillerBoard Clone()
         //{
