2afe2018ac99e5586ee3631a990034c1d4aac9b2
Tim Lawrence <timothyjl2002@gmail.com>
2022-12-28 10:31:34 -0700

    Fix minimax project

---
+++ Filler/IMinimaxBoard/IMinimaxBoard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimax;
+
+public interface IMinimaxBoard
+{
+    // move
+    public abstract void Move(int[] move);
+
+    // unmove
+    public abstract void Unmove();
+
+    // score position
+    public abstract int Score { get; }
+
+    public abstract byte Turn { get; protected set; }
+
+    // valid moves
+    public abstract List<int[]> ValidMoves();
+
+    //public abstract bool Won(out byte winner);
+}
